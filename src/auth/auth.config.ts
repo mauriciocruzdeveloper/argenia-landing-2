8bc6a3b5d26d4b6514b5a4a384ba2b9d0575ecc1
mauriciocruzdeveloper <mauricio11111@gmail.com>
2024-06-12 22:35:19 -0300

    chore: Remove console.log statements from auth.config.ts

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -9,6 +9,9 @@ import { User } from '../definitions/definitions';
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID ?? '';
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET ?? '';
 
+console.log('!!!GOOGLE_CLIENT_ID', GOOGLE_CLIENT_ID);
+console.log('!!!GOOGLE_CLIENT_SECRET', GOOGLE_CLIENT_SECRET);
+
 if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
   console.error('Google Client ID or Secret is missing.');
 }
