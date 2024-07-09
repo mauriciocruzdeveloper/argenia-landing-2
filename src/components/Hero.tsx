990464a325259f4e9fde7b864259f552de85606c
mauriciocruzdeveloper <mauricio11111@gmail.com>
2024-07-09 18:25:03 -0300

    feat: Update hero title and companies text in Spanish translation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,14 +19,34 @@ export default function Hero() {
   const companies = t("companies");
 
   return (
-    <>
-      <div className="px-5 pt-4 pb-8 lg:container lg:max-w-[846px] lg:m-auto">
-        <h1 className="text-info font-medium text-[32px] text-center lg:text-6xl lg:pt-11 lg:pb-6">
-          {title}
-        </h1>
-        <p className="text-primary text-center text-[16px] lg:text-[18px] mt-6">
-          {subtitle}
-        </p>
+    <div>
+      <div style={{ position: "relative" }}>
+        <Image
+          className="w-full object-cover"
+          height={500}
+          width={500}
+          layout="responsive"
+          src="/assets/hero-image.jpg"
+          alt="Hero Image"
+        />
+        <div
+          style={{
+            position: "absolute",
+            margin: "auto",
+            top: 0,
+            left: 0,
+            zIndex: 1, // Asegura que el contenido esté encima de la imagen
+          }}
+        >
+          <div className="flex flex-col justify-between px-5 pt-4 pb-8 lg:container m-auto align-middle">
+            <h1 className="text-info font-medium text-[32px] text-center lg:text-6xl lg:pt-11 lg:pb-6">
+              {title}
+            </h1>
+            <p className="text-primary text-center text-[16px] lg:text-[18px] mt-6">
+              {subtitle}
+            </p>
+          </div>
+        </div>
       </div>
       <div className="px-5 pb-2 lg:container flex justify-center items-center gap-x-6 lg:gap-x-10">
         <button
@@ -42,7 +62,7 @@ export default function Hero() {
         </Link> */}
       </div>
       <div
-        className={`bg-gradient-to-b from-white to-[#546EAB] h-full relative flex justify-center min-h-[450px] sm:min-h-[150px] w-full object-cover`}
+        className={`bg-gradient-to-b from-white to-[#546EAB] h-full relative flex justify-center min-h-[450px] lg:min-h-[150px] w-full object-cover`}
       >
         {/* <Image src={Gradient} className="min-h-[500px] sm:min-h-[600px] md:min-h-[700px] w-full object-cover lg:min-h-[800px]" alt="Gradient" /> */}
         <div className="absolute bottom-8 left-4 right-4 lg:container">
@@ -51,7 +71,7 @@ export default function Hero() {
             <p className="sm:text-white text-black text-[16px] lg:text-[18px] text-center pt-4 lg:pt-0 sm:pt-6 md:pt-8 my-4">
               {companies}
             </p>
-            <div className="flex flex-col sm:flex-row flex-wrap gap-x-8 gap-y-8 xl:gap-[96px] justify-center items-center">
+            <div className="flex flex-col lg:flex-row flex-wrap gap-x-8 gap-y-8 xl:gap-[96px] justify-center items-center">
               {/* <Image height={50} src={ReactLogo} alt="React" /> */}
               <Image height={30} src={RenderLogo} alt="Render" />
               <Image height={30} src={StrapiLogo} alt="Strapi" />
@@ -62,6 +82,6 @@ export default function Hero() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
